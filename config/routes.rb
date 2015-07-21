217c626fbff10422d79c7a0e630ee09b931719e5
Jason Edelstein <jasone@uchicago.edu>
2015-07-21 14:51:56 -0500

    A very small language tweak...

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   get '/places' => 'places#index'
   get '/places/new' => 'places#new'
-  post '/places' => 'places#submitted'
+  post '/places' => 'places#create'
   
   get '/places/:id' => 'places#show'
   get '/places/:id/edit' => 'places#edit'
   patch '/places/:id' => 'places#update'
 
-  delete '/places/:id' => 'places#delete'
+  delete '/places/:id' => 'places#destroy'
   
   post '/reviews/:place_id'  => 'reviews#create'
 
